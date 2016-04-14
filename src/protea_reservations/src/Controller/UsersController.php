0aa5daf71d55c75e0d803a0c81749445ea302ed9
franciscozm <baycfran42@gmail.com>
2016-04-14 14:50:14 -0600

    Se agrego registro parcialmente.

---
+++ src/protea_reservations/src/Controller/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+// src/Controller/UsersController.php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Event\Event;
+
+class UsersController extends AppController
+{
+
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow('add');
+    }
+
+     public function index()
+     {
+        $this->set('users', $this->Users->find('all'));
+    }
+
+    public function view($id)
+    {
+        $user = $this->Users->get($id);
+        $this->set(compact('user'));
+    }
+
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('Su solicitud ha sido procesada, espere la confirmación.'));
+                return $this->redirect(['action' => 'add']);
+            }
+            $this->Flash->error(__('No ha sido posible procesar su solicitud.'));
+        }
+        $this->set('user', $user);
+    }
+
+}
+
+?>
