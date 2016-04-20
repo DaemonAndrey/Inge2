2c245fe509f86d8f5dcd81e188cad58901bbdbc7
DaemonAndrey <robinperez38@gmail.com>
2016-04-20 00:06:25 -0600

    Se agregó un ejemplo para mails

---
+++ src/Mail/mail.php
@@ -0,0 +1,41 @@
+<?php
+
+
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+define('GUSER', 'proyecto.ecci.protea@gmail.com');
+define('GPWD', 'ecciprotea');
+
+global $error;
+
+$mail = new PHPMailer();
+
+$mail->isSMTP();
+
+$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true;  // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465; 
+$mail->Username = GUSER;  
+$mail->Password = GPWD;           
+$mail->SetFrom($_POST['from'], 'PROTEA');
+$mail->Subject = 'Hacked!!';
+$mail->Body = $_POST['message'];
+$mail->AddAddress($_POST['to']);
+/**$mail->AddCC('jocajime@gmail.com');
+$mail->AddCC('jfonseca1510@gmail.com');
+$mail->AddCC('baycfran42@gmail.com');
+$mail->AddCC('kakiandrea@gmail.com');
+$mail->AddCC('jedups95@gmail.com');
+$mail->AddCC('andresvm9@gmail.com');
+**/
+if(!$mail->Send()) {
+	$error = 'Mail error: '.$mail->ErrorInfo; 
+	return false;
+} else {
+	$error = 'Message sent!';
+	return true;
+}
+
+?>
