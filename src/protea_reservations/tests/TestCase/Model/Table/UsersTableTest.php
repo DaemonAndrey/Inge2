b0fef7f4f654bff73885ed68e51da51089fef947
franciscozm <baycfran42@gmail.com>
2016-04-21 09:22:43 -0600

    Agregados algunos dato de pruebas.

---
+++ src/protea_reservations/tests/TestCase/Model/Table/UsersTableTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\UsersTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+class UsersTableTest extends TestCase
+{
+    public $fixtures = ['app.users'];
+
+    public function testUserModel() 
+    {
+		$result = $this->loadFixtures('Users');
+		debug($result);
+	}
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Users = TableRegistry::get('Users');
+    }
+
+
+}
+
+>?
