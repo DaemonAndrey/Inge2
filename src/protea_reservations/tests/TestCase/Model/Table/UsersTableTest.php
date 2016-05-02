000bd30a90931b44ff4059dd7a20e40bd3d106a0
Andres Vargas <andresvm9@gmail.com>
2016-05-01 21:24:59 -0600

    Pruebas unitarias

--- src/protea_reservations/tests/TestCase/Model/Table/UsersTableTest.php
+++ src/protea_reservations/tests/TestCase/Model/Table/UsersTableTest.php
@@ -1,21 +1,58 @@
 <?php
-
 namespace App\Test\TestCase\Model\Table;
 
-use src\Model\Table\UsersTable;
+use App\Model\Table\UsersTable;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 
 class UsersTableTest extends TestCase
 {
-    public $fixtures = ['app.users'];
-    
+    public $Users;
+    public $fixtures = [
+        'app.users'
+    ];
+
     public function setUp()
     {
         parent::setUp();
-        $this->Users = TableRegistry::get('Users');
+        $config = TableRegistry::exists('Users') ? [] : ['className' => 'App\Model\Table\UsersTable'];
+        $this->Users = TableRegistry::get('Users', $config);
+    }
+
+    public function tearDown()
+    {
+        unset($this->Users);
+        parent::tearDown();
+    }
+
+    public function testValidationDefault()
+    {
+        $result = $this->loadFixtures('Users');
+		debug($result);
+    }
+
+    public function testRegistrationConfirmed()
+    {
+        $query = $this->Articles->findByState(1);
+        $this->assertInstanceOf('Cake\ORM\Query', $query);
+        $result = $query->hydrate(false)->toArray();
+        $expected = [
+            ['username' => 'Usuario1', 'password' => 'usuario1',
+              'first_name' => 'Usuario1',
+              'last_name' => 'Apellido1',
+              'telephone_number' => '88990099',
+              'department' => 'Educacion',
+              'position' => 'Docente',
+              'state' => '1'],
+        ];
+
+        $this->assertEquals($expected, $result);
+        
+    }
+    
+    public function testEqualtofield()
+    {
+        $result = $this->loadFixtures('Users');
+		//debug($result);
     }
 }
-
-?>
-
