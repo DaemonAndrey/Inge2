644e1a739504897257f3fed228a344843d9d1da3
Jose Jiménez <jocajime@gmail.com>
2016-06-09 00:03:47 -0600

    pruebas funcionales tipos de recursos días de anticipacion

---
+++ doc/Pruebas Funcionales/ITERACIÓN #3/ADMINISTRAR TIPOS DE RECURSOS/1.2 Agregar días en negativo.java
@@ -0,0 +1,90 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class 2 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost/protea/src/protea_reservations";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test2() throws Exception {
+    driver.get(baseUrl + "/protea/src/protea_reservations/");
+    driver.findElement(By.linkText("Ingresar")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("monica@ucr.ac.cr");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("monicamonica");
+    driver.findElement(By.cssSelector("button.btn.btn-info")).click();
+    driver.findElement(By.linkText("Tipos de Recurso")).click();
+    driver.findElement(By.linkText("Agregar tipo de recurso")).click();
+    driver.findElement(By.id("resourcetypes-description")).clear();
+    driver.findElement(By.id("resourcetypes-description")).sendKeys("Nuevo Recurso");
+    driver.findElement(By.id("resourcetypes-days-before-reservation")).clear();
+    driver.findElement(By.id("resourcetypes-days-before-reservation")).sendKeys("-5");
+    driver.findElement(By.cssSelector("button.btn.btn-info")).click();
+    for (int second = 0;; second++) {
+    	if (second >= 60) fail("timeout");
+    	try { if ("La cantidad debe ser mayor o igual a 0".equals(driver.findElement(By.cssSelector("div.error-message")).getText())) break; } catch (Exception e) {}
+    	Thread.sleep(1000);
+    }
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
